05254241bcf840897a08557e6be390820568c4d1
Oscarmdc <oscarg.mdcv@gmail.com>
2024-05-08 15:02:03 -0700

    Apps de los diferentes partes del proyecto

--- ProyectoDjango/settings.py
+++ ProyectoDjango/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'producto',
+    'categoria',
+    'ubicacion',
+    'proveedor',
+    'usuario',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +79,23 @@ WSGI_APPLICATION = 'ProyectoDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bdAlmacen',
+        'USER': 'postgre',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
